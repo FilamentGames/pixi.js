33e5482155afd1451ba4eb7a26632994f3814cc6
Alex Stone <stone@filamentgames.com>
2017-02-15 01:03:55 -0600

    Implement Y up with respect to the screen

--- src/core/renderers/webgl/utils/RenderTarget.js
+++ src/core/renderers/webgl/utils/RenderTarget.js
@@ -276,10 +276,10 @@ export default class RenderTarget
         else
         {
             pm.a = 1 / destinationFrame.width * 2;
-            pm.d = -1 / destinationFrame.height * 2;
+            pm.d = 1 / destinationFrame.height * 2;
 
-            pm.tx = -1 - (sourceFrame.x * pm.a);
-            pm.ty = 1 - (sourceFrame.y * pm.d);
+            pm.tx = -1 - sourceFrame.x * pm.a;
+            pm.ty = -1 - sourceFrame.y * pm.d;
         }
     }
 
